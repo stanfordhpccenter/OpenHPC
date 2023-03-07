cd3a1a2dae43ab276d664ea39ca4568f497903e7
Steve Jones <stevejones@stanford.edu>
2023-03-07 14:03:12 -0800

    Update recipe.sh

--- hpc-for-the-rest-of-us/recipes/rocky8/warewulf4/slurm/recipe.sh
+++ hpc-for-the-rest-of-us/recipes/rocky8/warewulf4/slurm/recipe.sh
@@ -207,7 +207,7 @@ dnf -y --installroot=$CHROOT install dmidecode parted grub2 numactl chrony
 
 systemctl --root=$CHROOT enable chronyd.service
 
-perl -pi -e "s/pool 2.rhel.pool.ntp.org iburst/server 10.1.1.1/" $CHROOT/etc/chrony.conf 
+perl -pi -e "s/pool 2.rocky.pool.ntp.org iburst/server 10.1.1.1/" $CHROOT/etc/chrony.conf 
 
 dnf -y --installroot=$CHROOT install lua lua-filesystem lua-posix
 
